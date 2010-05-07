6881ebc92f91b07c3899cf3e4824430e80084717
ji weifeng <weifeng.ji@gmail.com>
2010-05-07 16:40:02 +0800

    ttt.scm~

---
+++ ttt.scm
@@ -0,0 +1,29 @@
+(define (find-triples position)
+  (every (lambda (comb) (substitute-triple position)) '(123 456 789 147 258 369 159 357)))
+
+(define (substitute-triple combination position)
+  (accumulate word (every  (lambda (square)                             
+                             (substitute-letter square position)) combination)))
+
+(define (substitute-letter square position)
+  (if (equal? '_ (item square position))
+      square
+      (item square position)))
+
+
+(define (ttt-choose position me)
+  (ttt-choose (find-triples position) me))
+
+(define apprearances
+  (lambda (words string)
+    
+    
+    
+(define (my-pair? triple me)
+  (and (= (appearances me triple) 2)
+       (= (appearances (opponent me) triple) 0)))
+    
+ (define (opponent letter)
+   (if (equal? letter 'x) 'o 'x))
+    
+    
